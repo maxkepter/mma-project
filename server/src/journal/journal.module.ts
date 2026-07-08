04f9d3a2a2008713ca7240ee92383d82e7c8647c
Nguyen An <42490272+JuroMark@users.noreply.github.com>
2026-07-08 23:37:56 +0700

    feat: add JournalProcessor service for processing daily bets and managing bet results

--- server/src/journal/journal.module.ts
+++ server/src/journal/journal.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JournalService } from './journal.service';
+import { JournalProcessor } from './services/journal.processor';
 import { JournalController } from './journal.controller';
 import { BetEntry } from './entities/bet-entry.entity';
 import { BetResult } from './entities/bet-result.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BetEntry, BetResult])],
-  providers: [JournalService],
+  providers: [JournalService, JournalProcessor],
   controllers: [JournalController],
   exports: [TypeOrmModule, JournalService],
 })
